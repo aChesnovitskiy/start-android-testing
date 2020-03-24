97b4843f602086f8893748b7df6a17e332ae6d2f
aChesnovitskiy <a.chesnovitskiy@gmail.com>
2020-03-24 05:46:41 +0300

    finish lesson 4

--- app/src/main/java/ru/startandroid/testing/data/order/Order.kt
+++ app/src/main/java/ru/startandroid/testing/data/order/Order.kt
@@ -1,4 +1,7 @@
 package ru.startandroid.testing.data.order
 
-data class Order(val id: Long, val amount: Float, val description: String) {
-}
+data class Order(
+    val id: Long,
+    val amount: Float,
+    val description: String
+)
